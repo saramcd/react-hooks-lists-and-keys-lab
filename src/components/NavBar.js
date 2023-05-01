4b25a4cdafd830a7037684c022ebc0edaced86d8
Sara McDonnell <saramcdonnell32@gmail.com>
2023-05-01 17:06:39 -0400

    lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linkAnchorTags = links.map((link) => {
+    return (
+      <a href={"#" + link} key={link}>
+        {link}
+      </a>
+    );
+  });
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{linkAnchorTags}</nav>;
 }
 
 export default NavBar;
